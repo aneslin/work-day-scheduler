d9b8f8158d355458a86141ef6e563f54e1d972a3
Aaron Neslin <aneslin125@gmail.com>
2021-12-23 13:31:45 -0500

    highligh past future present

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-/* display current date */ 
-
+//const now = 16
+const now = moment().hour()
 let today = moment().format('MMMM Do YYYY');
-console.log(today)
-$("#currentDay").text(today);
+
+/* var textareas = document.body.getElementsByTagName("textarea")
+function setColors(){
+for(let i = 0; i < textareas.length; i++){
+    let x  = parseInt(textareas[i].getAttribute("data-hour"))
+    if (x > now){
+        console.log(x + " is in the future")
+        textareas[i].classList.add("future")
+    }
+    else if (x === now){
+        textareas[i].classList.add("present")
+    } else { textareas[i].classList.add("past")} 
+};
+}
+ */
+
+function setColors(){
+$("textarea").each(function(){
+let x = $(this).data("hour")
+    x = parseInt(x)
+    if (x > now){
+        $(this).addClass("future")}
+    else if (x === now){
+        $(this).addClass("present")
+    } else{ $(this).addClass("past")}
+    }
+)
+}
+
+console.log($("button"))
+
+/* add data element to text 
+ * use > < = to apply css */
+
